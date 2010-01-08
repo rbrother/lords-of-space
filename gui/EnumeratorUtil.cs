e2bd3c60082b1f1f5a0cfa5856aaac8be2612b92
U-atom\bunnies <robert@iki.fi>
2010-01-08 08:36:03 +0200

    new files

---
+++ gui/EnumeratorUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Net.Brotherus
+{
+    public static class EnumeratorUtil
+    {
+        public static List<object> EnumeratorToList(IEnumerator e)
+        {
+            var l = new List<object>();
+            if (e != null)
+            {
+                while (e.MoveNext())
+                {
+                    l.Add(e.Current);
+                }
+            }
+            return l;
+        }
+    }
+}
